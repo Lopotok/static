24e60060a48825dd7a64089c0c8c3b46e39fc193
Bonivárt Krisztián <krisztian.bonivart@gmail.com>
2023-06-17 11:03:28 +0200

    Static Feladat

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         // írd meg a person osztályt
+        // példányosísd 3 szor a
 
-        // példányosísd 3 szor a persont
+        Person person = new Person();
+        Person person1 = new Person();
+        Person person2 = new Person();
+
 
         // kérd le a statikus fieldet
+
+        System.out.println(Person.popular);
 
         // példányosísd még 1 szer
+        Person person3 = new Person();
 
         // kérd le a statikus fieldet
+        System.out.println(Person.popular);
 
         // hívd meg a katasztofa metódust
+        System.out.println(Person.katasztrofa());
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,9 +1,24 @@
 public class Person {
     // írj egy statikus fieldet popular néven
 
+     static int popular;
+
     // írj egy konstruktort ami megnöveli a statikus fieldünket
+
+    public Person() {
+        popular++;
+    }
+
 
     // írj egy metódust ami vissza adja a popular értékét
 
+    public static int getPopular(){
+        return popular;
+    }
+
     // írj egy katasztrófa statikus metódust ami nulláza a populart
+
+    public static int katasztrofa() {
+        return popular = 0;
+    }
 }
